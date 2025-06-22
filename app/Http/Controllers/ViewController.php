92fabb0cf9d06fca4a7eeca79348a30b962c4a53
unknown <thephenome1998@gmail.com>
2025-06-22 14:54:53 +0700

    first commit

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+
+class ViewController extends Controller
+{
+    public function homeView()
+    {
+        $tripGallery = File::files(public_path('img/trip-gallery'));
+        $tripGallery = collect($tripGallery)->map(function ($file) {
+            return asset('img/trip-gallery/' . $file->getFilename());
+        });
+        return view('home', compact('tripGallery'));
+    }
+}
